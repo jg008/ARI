80402c78959460e2720b2ed4e6783b24b7edf205
jg008 <joaonc176@gmail.com>
2025-09-02 07:51:15 -0300

    Update client.js

--- client.js
+++ client.js
@@ -5,13 +5,34 @@ const os = require ('os');
 const PORT = 5000;
 const HOST = '10.91.236.143';
 
-const client = new net.Socket();
+const client = new net.Socket()
 
-const rl = readline.createInterfaxe({
-      input: process.stdin,
-      output: process.stdout
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+function perguntar(){
+    rl.question("Digite sua mensagem(ou 'sair' para encerrar): ", (msg) => {
+        if (msg.toLowerCase() === "sair") {
+            client.end();
+            rl.close();
+            return;
+        }
+        client.write(`${os.userInfo().username}: ${msg}`);
+        perguntar();
+    });
+}
+
+client.connect(PORT, HOST, () => {
+    console.log(`Conectado ao servidor TCP!`);
+    perguntar();
 });
 
-function perguntar (){
-    rl.question('Digite sua mensagem (ou 'sair' para encerrar): ", (msg) => 
-}
+client.on("data", (data) => {
+    console.log(`Resposta do servidor: ${data.toString()}`);
+})
+
+client.on("close", () => {
+    console.log("Conexão encerrada pelo servidor.");
+});
